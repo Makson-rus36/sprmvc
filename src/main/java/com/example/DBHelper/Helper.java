9deafb7161f560d3eb8e35cf637fc3349a65f184
Максим Тюкин <maksim.t8600@gmail.com>
2020-11-14 15:25:28 +0300

    ''

--- src/main/java/com/example/DBHelper/Helper.java
+++ src/main/java/com/example/DBHelper/Helper.java
@@ -11,7 +11,7 @@ public class Helper {
     Class.forName("org.postgresql.Driver");
 
     //connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/lrservlet?verifyServerCertificate=false&useSSL=false&requireSSL=false&useLegacyDatetimeCode=false&amp&serverTimezone=UTC");
-    connection = DriverManager.getConnection("jdbc:postgresql://otedgrtgkygbxq:aacc54e171cda6acbbd51c2ef19ab8ddc4495f66294d8e41d41a96517e635dad@ec2-54-246-115-40.eu-west-1.compute.amazonaws.com:5432/dup8k9l5i750f?sslmode=require","otedgrtgkygbxq", "aacc54e171cda6acbbd51c2ef19ab8ddc4495f66294d8e41d41a96517e635dad");
+    connection = DriverManager.getConnection("jdbc:postgresql://otedgrtgkygbxq:aacc54e171cda6acbbd51c2ef19ab8ddc4495f66294d8e41d41a96517e635dad@ec2-54-246-115-40.eu-west-1.compute.amazonaws.com:5432/dup8k9l5i750f?ssl=true&sslmode=require");
     Statement stat = connection.createStatement();
     ResultSet rs = stat.executeQuery("select id,_name,_lastname " +
             "from users " +
